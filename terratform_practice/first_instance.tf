86960e6c7624c60c66441b72d28c4dc2a01ff248
maneshb4u <159014734+maneshb4u@users.noreply.github.com>
2024-08-22 10:56:46 +0530

    Update first_instance.tf

--- terratform_practice/first_instance.tf
+++ terratform_practice/first_instance.tf
@@ -1,1 +1,14 @@
+provider "aws" {
+  region     = "us-east-1"
+  access_key = "AKIAVKF75RDVSFFPVLN"
+  secret_key = "AEFnw+klX/uj/tNy7E04S9VKYoBYgOhe9SslR2uP"
+}
 
+resource "aws_instance" "ec2_1" {
+  ami           = "ami-0e86e20dae9224db8"
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "First_instance"
+  }
+}
